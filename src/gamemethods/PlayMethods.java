653459ab1bd60bc2adfabc41494785ea069bbdc4
Sebastian Andrade <sebasandroide05@gmail.com>
2021-07-01 11:15:12 -0500

    Changes on Playmethods to play more intelligent

--- src/gamemethods/PlayMethods.java
+++ src/gamemethods/PlayMethods.java
@@ -15,34 +15,36 @@ public final class PlayMethods {
         }
     }
 
-    public static void clearConsole(){
-        try {
-            final String os = System.getProperty("os.name").toLowerCase();
-            
-            if(os.contains("mac") || os.contains("linux")){
-                Runtime.getRuntime().exec("clear");
-            }
+    //All these methods from here to the end are for the pc's playing engine
 
-        } catch (Exception e) {
-            
-            System.out.println("AAAAAAAAAAAAAA");
-        }    
-    }
-
-    public static void selectPosition(char[][] board){
-        //This func uses the board state to decide how to play
-        int myRow = 5;
-        int myColumn = 5;
+    private static List<Integer[]> getPositions(char[][] board, char who){
+        /**
+        Given a board and a character to look for this method will return an ArrayList with Integer arrays inside it filled
+        with the coordinates where are the given character 
+        **/
         List<Integer[]> positions = new ArrayList<Integer[]>();
 
         for(int row = 0; row < 3; row++){
 			for(int column = 0; column < 3; column++ ){
-				if(board[row][column] == 'X'){
+				if(board[row][column] == who){
 					Integer[] newPos = {row, column};
 					positions.add(newPos);
 				}
 			}
 		}
+
+        return positions;
+
+    }
+
+
+    private static int[] detectThreat(char[][] board){
+        /**
+          Using the board this method will find threats and give us the coordinates where whe have to put our O to dont let the user win
+          if there are no threats the method will return an array filled with -1
+        **/
+        int[] threat = {-1, -1};
+        List<Integer[]> positions = getPositions(board, 'X');
 
         if(positions.size() >= 2){
             for(int i = 0; i < positions.size() - 1; i++){
@@ -60,9 +62,9 @@ public final class PlayMethods {
                             b = Math.abs(firstPos[1] - secondPos[1]) - 1;
                         }
 
-                        if(board[a][b] == '*' && myRow == 5 && myColumn == 5){
-                            myRow = a;
-                            myColumn = b;
+                        if(board[a][b] == '*' && threat[0] == -1 && threat[1] == -1){
+                            threat[0] = a;
+                            threat[1] = b;
                         }
                     
                     }else if(firstPos[1] == secondPos[1]){
@@ -75,32 +77,38 @@ public final class PlayMethods {
                             a = Math.abs(firstPos[0] - secondPos[0]) - 1;
                         }
 
-                        if(board[a][b] == '*' && myRow == 5 && myColumn == 5){
-                            myRow = a;
-                            myColumn = b;
+                        if(board[a][b] == '*' && threat[0] == -1 && threat[1] == -1){
+                            threat[0] = a;
+                            threat[1] = b;
                         }
 
                     }else if((firstPos[0] + firstPos[1] == 0 || firstPos[0] + firstPos[1] == 2 || firstPos[0] + firstPos[1] == 4) && firstPos[0] != 1){
                         int a = Math.abs(firstPos[0] - 2);
                         int b = Math.abs(firstPos[1] - 2);
         
-                        if(board[a][b] == '*' && myRow == 5 && myColumn == 5){
-                            myRow = a;
-                            myColumn = b;
+                        if(board[a][b] == '*' && threat[0] == -1 && threat[1] == -1){
+                            threat[0] = a;
+                            threat[1] = b;
                         }
                     
                     }else if((secondPos[0] + secondPos[1] == 0 || secondPos[0] + secondPos[1] == 2 || secondPos[0] + secondPos[1] == 4) && secondPos[0] != 1){
                         int a = Math.abs(secondPos[0] - 2);
                         int b = Math.abs(secondPos[1] - 2);
         
-                        if(board[a][b] == '*' && myRow == 5 && myColumn == 5){
-                            myRow = a;
-                            myColumn = b;
+                        if(board[a][b] == '*' && threat[0] == -1 && threat[1] == -1){
+                            threat[0] = a;
+                            threat[1] = b;
                         }
                     }
                 }
             }
         
+            /** 
+            Threats algorithm will work even if there is just one position because if the user plays a corner a smart move 
+            could be play the opossite corner, also if the center is free we would like to move there because it will probably
+            help us to find oportunities in a future. So these user moves are considered threats.
+            **/
+
         }else if(positions.size() == 1){
             Integer[] position = positions.get(0);
 
@@ -109,36 +117,132 @@ public final class PlayMethods {
                 int b = Math.abs(position[1] - 2);
 
                 if(board[a][b] == '*'){
-                    myRow = a;
-                    myColumn = b;
+                    threat[0] = a;
+                    threat[1] = b;
                 }
 
             }else if(board[1][1] == '*'){
-                myRow = 1;
-                myColumn = 1;
+                threat[0] = 1;
+                threat[1] = 1;
             
-            }else{
-                myRow = (int)Math.floor(Math.random()*(2-0+1)+0);
-                myColumn = (int)Math.floor(Math.random()*(2-0+1)+0);
+            }
+        
+        }
 
-                while(board[myRow][myColumn] != '*'){
-                    myRow = (int)Math.floor(Math.random()*(2-0+1)+0);
-                    myColumn = (int)Math.floor(Math.random()*(2-0+1)+0);
+        return threat;
+    }
+
+
+    private static int[] detectOpportunity(char[][] board){
+        /**
+          Using the board this method will find opportunities to win and return an array with the coordinates where we have
+          to put our O to win, if there are no opportunities the method will return an array filled with -1 
+        **/
+        int[] opportunity = {-1, -1};
+        List<Integer[]> positions = getPositions(board, 'O');
+
+        if(positions.size() >= 2){
+            for(int i = 0; i < positions.size() - 1; i++){
+                Integer[] firstPos = positions.get(i); 
+                for(int j = i + 1; j < positions.size(); j++){
+                    Integer[] secondPos = positions.get(j);
+
+                    if(firstPos[0] == secondPos[0]){
+                        int a = firstPos[0];
+                        int b;
+
+                        if(firstPos[1] + secondPos[1] == 1){
+                            b = 2;
+                        }else{
+                            b = Math.abs(firstPos[1] - secondPos[1]) - 1;
+                        }
+
+                        if(board[a][b] == '*' && opportunity[0] == -1 && opportunity[1] == -1){
+                            opportunity[0] = a;
+                            opportunity[1] = b;
+                        }
+                    
+                    }else if(firstPos[1] == secondPos[1]){
+                        int b = firstPos[1];
+                        int a;
+
+                        if(firstPos[0] + secondPos[0] == 1){
+                            a = 2;
+                        }else{
+                            a = Math.abs(firstPos[0] - secondPos[0]) - 1;
+                        }
+
+                        if(board[a][b] == '*' && opportunity[0] == -1 && opportunity[1] == -1){
+                            opportunity[0] = a;
+                            opportunity[1] = b;
+                        }
+
+                    }else if((firstPos[0] + firstPos[1] == 0 || firstPos[0] + firstPos[1] == 2 || firstPos[0] + firstPos[1] == 4) && firstPos[0] != 1){
+                        int a = Math.abs(firstPos[0] - 2);
+                        int b = Math.abs(firstPos[1] - 2);
+        
+                        if(board[a][b] == '*' && opportunity[0] == -1 && opportunity[1] == -1){
+                            opportunity[0] = a;
+                            opportunity[1] = b;
+                        }
+                    
+                    }else if((secondPos[0] + secondPos[1] == 0 || secondPos[0] + secondPos[1] == 2 || secondPos[0] + secondPos[1] == 4) && secondPos[0] != 1){
+                        int a = Math.abs(secondPos[0] - 2);
+                        int b = Math.abs(secondPos[1] - 2);
+        
+                        if(board[a][b] == '*' && opportunity[0] == -1 && opportunity[1] == -1){
+                            opportunity[0] = a;
+                            opportunity[1] = b;
+                        }
+                    }
                 }
             }
         }
 
-        if(myRow == 5 && myColumn == 5){
-            myRow = (int)Math.floor(Math.random()*(2-0+1)+0);
-            myColumn = (int)Math.floor(Math.random()*(2-0+1)+0);
+        /**
+        opportunity algorithm will only work if there are two or more pc positions
+        **/
 
-                while(board[myRow][myColumn] != '*'){
-                    myRow = (int)Math.floor(Math.random()*(2-0+1)+0);
-                    myColumn = (int)Math.floor(Math.random()*(2-0+1)+0);
-                }
+        return opportunity;
+    }
+
+
+    public static void selectPosition(char[][] board){
+        //This func uses the board state to decide how to play
+
+        int[] threat = detectThreat(board);
+        int[] opportunity = detectOpportunity(board);
+
+        if(opportunity[0] != -1 && opportunity[1] != -1){ //If we have an opportunity to win
+            board[opportunity[0]][opportunity[1]] = 'O';
+        
+        }else if(threat[0] != -1 && threat[1] != -1){ //If we dont have an opportunity to win but we do have a threat
+            board[threat[0]][threat[1]] = 'O';
+        
+        }else{ //If we dont have an opportunity to win neither a threat
+
+            int myRow = (int)Math.floor(Math.random()*(2-0+1)+0);
+            int myColumn = (int)Math.floor(Math.random()*(2-0+1)+0);
+            board[myRow][myColumn] = 'O';
         }
 
-        board[myRow][myColumn] = 'O';
     }
+
+
+    public static void main(String[] args){
+        char[][] board = {
+            {'O', '*', '*'},
+            {'X', 'X', '*'},
+            {'*', '*', '*'}
+        };
+
+        printBoard(board);
+        selectPosition(board);
+        System.out.println(" ");
+        printBoard(board);
+
+    }
+
+    
 
 }
